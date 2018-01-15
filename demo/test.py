a19a8c3025d73aaa21f2282e40ec00005680813a
wangyan02 <13758216976@163.com>
2018-01-15 14:59:58 +0800

    git_test

---
+++ demo/test.py
@@ -0,0 +1,16 @@
+
+from selenium import webdriver
+import  time
+
+
+dr = webdriver.Chrome("chromedriver.exe")
+dr.get('https://www.baidu.com/')
+time.sleep(1)
+dr.close()
+poem = '''
+testgit
+'''
+with open('git_test.txt', 'wt') as fout:
+   fout.write(poem)
+
+
