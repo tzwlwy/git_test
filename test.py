150f85bcf0d5d2332f7e2f5ee8dbc336547f8023
wangyan02 <13758216976@163.com>
2018-01-15 17:37:21 +0800

    git_test

--- test.py
+++ test.py
@@ -1,15 +1,13 @@
 
 from selenium import webdriver
-import  time
-
-
+import time
+import os
+import sys
 dr = webdriver.Chrome("chromedriver.exe")
 dr.get('https://www.baidu.com/')
 time.sleep(1)
 dr.close()
-poem = '''
-testgit
-'''
+a=os.getcwd()
 with open(r'D:\PHOTO\曾松点开看看123.txt', 'wt') as fout:
-   fout.write(poem)
+   fout.write(a)
 
