7cff343333f31896f1bb4653dd150b6cd9d7c88f
wangyan02 <13758216976@163.com>
2018-01-15 17:29:34 +0800

    git_test

--- demo/test.py
+++ demo/test.py
@@ -8,9 +8,9 @@ dr.get('https://www.baidu.com/')
 time.sleep(1)
 dr.close()
 poem = '''
-testgit
+曾松，你好
 '''
-with open(r'D:\PHOTO\git_test.txt', 'wt') as fout:
+with open(r'D:\PHOTO\曾松点开看看.txt', 'wt') as fout:
    fout.write(poem)
 
 
